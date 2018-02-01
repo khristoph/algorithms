dd665cdd489622698b761832cd009606f9a9821b
khristoph <christopherfriedman@gmail.com>
2018-01-31 18:31:27 -0600

    sorting challenge tutorial

---
+++ sorting/intro_to_tutorial_challenges.rb
@@ -0,0 +1,10 @@
+def introTutorial(v, arr)
+    arr.index(v)
+end
+
+# v = gets.strip.to_i
+# n = gets.strip.to_i
+# arr = gets.strip
+# arr = arr.split(' ').map(&:to_i)
+# result = introTutorial(v, arr)
+# puts result
